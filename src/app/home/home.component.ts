7c81aee4b11a2ae8e498959702194417d3b5cbdc
Brandon Taylor <alsoicode@gmail.com>
2017-04-16 14:02:29 -0400

    Home Component
    Added markup and styling.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,16 @@ import { Component } from '@angular/core';
   styleUrls: [
     './home.component.scss',
   ],
-  template: `Hi`,
+  template: `
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-sm-12 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4">
+          <h1>Hi</h1>
+          <button class="btn btn-primary btn-lg btn-block">Sign In via Github <i class="fa fa-github"></i></button>
+          <a class="btn btn-link" href="https://github.com/join?source=header-home" target="_blank">I don&#8217;t have a Github Account</a>
+        </div>
+      </div>
+    </div>
+  `,
 })
 export class HomeComponent {}
