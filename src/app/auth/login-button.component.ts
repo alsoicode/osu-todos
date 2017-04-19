72ce7e2ddedb75ce780ec8fb92596ed3ef6e5343
Brandon Taylor <alsoicode@gmail.com>
2017-04-18 20:51:16 -0400

    Misc Styling
    Increased height of footer for xs viewports.
    Fixed column width of home content on small viewports.
    Reduced size of home headline for xs viewports.
    Moved variables into a separate file for sharing.

--- src/app/auth/login-button.component.ts
+++ src/app/auth/login-button.component.ts
@@ -4,6 +4,9 @@ import { AuthService } from './auth.service';
 
 @Component({
   selector: 'login-button',
+  styles: [
+    '.btn.btn-primary { margin-bottom: 0.5rem; }'
+  ],
   template: `
     <button class="btn btn-primary btn-lg btn-block" (click)="onLogin()">Sign In via Github <i class="fa fa-github"></i></button>
   `
