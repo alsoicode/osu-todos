1e117e75b3976ac57eca70a6c4958d4f9bac19b0
Brandon Taylor <alsoicode@gmail.com>
2017-04-18 20:27:37 -0400

    Sign In Link
    Removed un-necessary querystring parameter.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
         <div class="col-sm-12 col-md-4 col-md-offset-4 col-lg-4 col-lg-offset-4">
           <h1>Hi</h1>
           <login-button></login-button>
-          <a class="btn btn-link" href="https://github.com/join?source=header-home" target="_blank">I don&#8217;t have a Github Account</a>
+          <a class="btn btn-link" href="https://github.com/join" target="_blank">I don&#8217;t have a Github Account</a>
         </div>
       </div>
     </div>
