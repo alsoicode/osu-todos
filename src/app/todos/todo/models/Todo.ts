76540fe0501b4842a9f5b6464b66e113aad88cce
Brandon Taylor <alsoicode@gmail.com>
2017-05-06 11:06:12 -0400

    Date Warning
    Added simple getter to display an icon if the todo is 3 days or older.

--- src/app/todos/todo/models/Todo.ts
+++ src/app/todos/todo/models/Todo.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { ITodo } from './ITodo';
 
 /**
@@ -11,4 +12,15 @@ export class Todo implements ITodo {
   key: string;
   createdOn: number;
   text: string;
+
+  /**
+   * This property demonstrates a very simple example of  why you would
+   * use your own object populated from Firebase data. You can add whatever
+   * getters, setters, etc.
+   */
+  get displayDateWarning(): boolean {
+    const now = moment();
+    const createdOnDate = moment(this.createdOn);
+    return now.diff(createdOnDate, 'days') > 3;
+  }
 }
